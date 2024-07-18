842c5c5d075adeb3d25a4f48e337a60b14d0684a
Nox <code.xexsteros@gmail.com>
2024-07-18 16:43:13 +0000

    completed the game!

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,74 +1,52 @@
+import random
 
-def get_ans ():
-  while True : 
-   try:
-    ans = int(input("Choose one of the following : "))
-   except  ValueError:
-    print("Enter the numbers you stupid! Try again!")
-   else :
-    break
+def get_user_choice():
+    # Function to get user's choice
+    choices = ['rock', 'paper', 'scissors']
+    while True:
+        print("\n--- Rock, Paper, Scissors ---")
+        print("Enter your choice:")
+        print("1. Rock")
+        print("2. Paper")
+        print("3. Scissors")
+        choice = input("Your choice (1/2/3): ").strip()
+        if choice in ['1', '2', '3']:
+            return choices[int(choice) - 1]
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
 
+def get_computer_choice():
+    # Function to get computer's choice
+    choices = ['rock', 'paper', 'scissors']
+    return random.choice(choices)
 
-  if ans == 1 or 2 or 3:
-   pass
-  else : get_ans()
-  
-  if ans == 1 :
-   move = "Rock"
-  elif ans == 2 :
-   move = "Paper"
-  else : move = "Scissors"
-  return move
- 
+def determine_winner(user_choice, computer_choice):
+    # Function to determine the winner
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'paper' and computer_choice == 'rock') or \
+         (user_choice == 'scissors' and computer_choice == 'paper'):
+        return "You win!"
+    else:
+        return "Computer wins!"
 
+def play_game():
+    print("Welcome to Rock, Paper, Scissors!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        
+        print(f"\nYou chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}\n")
+        
+        print(determine_winner(user_choice, computer_choice))
+        
+        play_again = input("\nDo you want to play again? (yes/no): ").strip().lower()
+        if play_again != 'yes':
+            break
 
- 
-def bot():
-  bmove = ""
-  count = range(3)
-  
-  if count == 1 :
-   bmove = "Rock"
-  elif count == 2 :
-   bmove = "Paper"
-  else : bmove = "Scissors"
-  return bmove
- 
-print("1.Rock")
-print("2.Paper")
-print("3.Scissors")
+    print("\nThanks for playing Rock, Paper, Scissors!")
 
-# Game
-
-player_ans = get_ans()
-bot_ans = bot()
-
-if player_ans and bot_ans ==  "Rock" :
- 
- print("So the match is equal")
-
-elif player_ans and bot_ans == "Paper":
- 
- print("So the match is equal")
-
-elif player_ans and bot_ans == "Scissors":
- 
- print("So the match is equal")
-
-
-elif player_ans == "Rock" and bot_ans == "Paper":
-
-    print("The bot won!")
-
-
-elif player_ans == "Rock" and bot_ans == "Scissors":
-
-    print("You won!")
-
-elif player_ans == "Paper" and bot_ans == "Scissors":
-
-    print("The bot won!")
-
-
-
-
+# Run the game
+play_game()
